50f73fbe82de7eba45a52f94aacd7b195d2b4eec
paul.kelly <pkelly@equalexperts.com>
2014-06-07 21:42:54 +0100

    Added validations for special characters.

--- app/scripts/models/password.js
+++ app/scripts/models/password.js
@@ -16,11 +16,18 @@ PasswordStrength.Models = PasswordStrength.Models || {};
         },
 
         validate: function(attributes) {
+            
             var errors = {};
+            var passwordRegularExpression = /^(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/;
+
             if (attributes.password.length < 8) {
                 errors.length = 'Password is less than 8 characters';
             }
             
+            if (!passwordRegularExpression.test(attributes.password)) {
+                errors.special = 'Password should contain at least one special character and any letter'
+            }
+
             if (_.keys(errors).length > 0) {
                 return errors;
             }
--- test/spec/test.js
+++ test/spec/test.js
@@ -9,17 +9,57 @@
                 this.model = new PasswordStrength.Models.PasswordModel();
             });
 
-            it('return an error if password length is less than 8 characters', function() {
+            it('should return an error if password length is less than 8 characters', function() {
 
                 var eventSpy = sinon.spy();
-				this.model.bind("error", eventSpy);
-                
-                this.model.set({password: '1234567'}, {validate: true});
+                this.model.bind('invalid', eventSpy);
 
-               	var validationValues = _.values(this.model.validationError);
+                this.model.set({
+                    password: '1234567'
+                }, {
+                    validate: true
+                });
+
+                var validationValues = _.values(this.model.validationError);
                 expect(validationValues[0]).toEqual('Password is less than 8 characters');
+
+                expect(eventSpy.calledOnce).toBeTruthy();
+
             });
 
+            it("should return an error if password does not contain a special character", function() {
+
+                var eventSpy = sinon.spy();
+                this.model.bind('invalid', eventSpy);
+
+                this.model.set({
+                    password: '12345678'
+                }, {
+                    validate: true
+                });
+
+                var validationValues = _.values(this.model.validationError);
+                expect(validationValues[0]).toEqual('Password should contain at least one special character and any letter');
+
+                expect(eventSpy.calledOnce).toBeTruthy();
+
+            });
+
+            it("should update model successfully when password is greater than 8 characters and contains at least 1 letter and a special character", function() {
+
+                var eventSpy = sinon.spy();
+                this.model.bind('invalid', eventSpy);
+
+                this.model.set({
+                    password: 'paulkelly$'
+                }, {
+                    validate: true
+                });
+
+                var validationValues = _.values(this.model.validationError);
+                expect(validationValues.length).toBe(0)
+                expect(eventSpy.notCalled).toBeTruthy();
+            });
         });
     });
 })();
